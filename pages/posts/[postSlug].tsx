c76b146b5eda04e0c6694df2cc13c1237c2d84bb
tarchevsky <i.tarchevsky@gmai.com>
2024-06-07 18:14:41 +0300

    Картинка статьи с данными статьи, удаление старых запросов к базе данных, цвет текста

--- pages/posts/[postSlug].tsx
+++ pages/posts/[postSlug].tsx
@@ -5,6 +5,7 @@ import { getApolloClient } from '@/lib/apollo-client'
 
 import { Params, PostEdge, PostPageProps } from '@/types'
 import Meta from '@/components/meta/Meta'
+import FadeIn from '@/components/fadeIn/FadeIn'
 
 export default function Post({ post, site }: PostPageProps) {
 	const { seo, featuredImage } = post
@@ -12,116 +13,42 @@ export default function Post({ post, site }: PostPageProps) {
 	return (
 		<div>
 			<Meta seo={seo} />
-
-			<main className='cont'>
-				<img src={featuredImage.node.link} alt={featuredImage.node.altText} />
-				<h1>{post.title}</h1>
-
-				<div>
-					<div
-						dangerouslySetInnerHTML={{
-							__html: post.content
-						}}
-					/>
+			<FadeIn className='relative'>
+				<img
+					src={featuredImage.node.link}
+					alt={featuredImage.node.altText}
+					className='h-[90svh] w-full object-cover brightness-50'
+				/>
+				<Link
+					href='/'
+					className='absolute top-[16px] left-[16px] sm:top-[40px] sm:left-[40px] md:top-[40px] md:left-[100px] 2xl:left-[200px]  text-white'
+				>
+					&lt; Назад
+				</Link>
+				<div className='absolute bottom-[16px] left-[16px] sm:bottom-[40px] sm:left-[40px] md:bottom-[40px] md:left-[100px] 2xl:left-[200px] flex flex-col'>
+					<h1 className='text-white'>{post.title}</h1>
+					<div className='text-white'>
+						Дата: <span>10.10.2010</span>
+					</div>
+					<div className='text-white'>
+						Рубрика: <span>Блог</span>
+					</div>
 				</div>
-
-				<p>
-					<Link href='/'>&lt; Back To Home</Link>
-				</p>
-			</main>
+			</FadeIn>
+			<FadeIn className='px-[16px] md:px-[400px]'>
+				<main>
+					<div>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: post.content
+							}}
+						/>
+					</div>
+				</main>
+			</FadeIn>
 		</div>
 	)
 }
-
-// export async function getStaticProps({ params }: { params: Params }) {
-// 	const apolloClient = getApolloClient()
-//
-// 	const data = await apolloClient.query({
-// 		query: gql`
-// 			query PostBySlug($slug: String!) {
-// 				generalSettings {
-// 					title
-// 				}
-// 				postBy(slug: $slug) {
-// 					id
-// 					content
-// 					title
-// 					slug
-// 					seo {
-// 						title
-// 						metaDesc
-// 					}
-// 					featuredImage {
-// 						node {
-// 							link
-// 							altText
-// 						}
-// 					}
-// 				}
-// 			}
-// 		`,
-// 		variables: {
-// 			slug: params.postSlug
-// 		}
-// 	})
-//
-// 	let post = data?.data.postBy
-//
-// 	const postData = {
-// 		...data?.data.postBy.feauturedImage
-// 	}
-//
-// 	const site = {
-// 		...data?.data.generalSettings,
-// 		...data?.data.seo
-// 	}
-//
-// 	return {
-// 		props: {
-// 			post,
-// 			postData,
-// 			path: `/posts/${post.slug}`,
-// 			site
-// 		},
-// 		revalidate: 10
-// 	}
-// }
-//
-// export async function getStaticPaths() {
-// 	const apolloClient = getApolloClient()
-//
-// 	const data = await apolloClient.query({
-// 		query: gql`
-// 			{
-// 				posts(first: 10000) {
-// 					edges {
-// 						node {
-// 							id
-// 							title
-// 							content
-// 							slug
-// 						}
-// 					}
-// 				}
-// 			}
-// 		`
-// 	})
-//
-// 	const posts = data?.data.posts.edges.map(({ node }: PostEdge) => node)
-//
-// 	const paths = posts.map(({ slug }: any) => {
-// 		return {
-// 			params: {
-// 				postSlug: slug
-// 			}
-// 		}
-// 	})
-//
-// 	return {
-// 		paths: [...paths],
-// 		fallback: 'blocking'
-// 	}
-// }
 
 export async function getStaticProps({ params }: { params: Params }) {
 	const { postSlug } = params
