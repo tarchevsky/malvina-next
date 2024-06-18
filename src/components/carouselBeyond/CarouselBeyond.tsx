7ee352e143554140f344f3309ca838e6b565c82a
tarchevsky <i.tarchevsky@gmai.com>
2024-06-18 15:41:08 +0300

    Удаление заголовков и лишних стилей из CarouselBeyond

--- src/components/carouselBeyond/CarouselBeyond.tsx
+++ src/components/carouselBeyond/CarouselBeyond.tsx
@@ -6,7 +6,28 @@ const CarouselBeyond = () => {
 			<div className='carousel carousel-center flex p-4 gap-5 mb-60'>
 				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
 					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
+						<p>Выздоравливает не умный, а дисциплинированный</p>
+					</div>
+				</div>
+				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
+					<div className='p-4'>
+						<p>
+							Каждое утро ты должен делать выбор, кем ты будешь сегодня. Ты
+							можешь закончить день разочарованным и безразличным, либо
+							уставшим, но на шаг ближе к цели
+						</p>
+					</div>
+				</div>
+				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
+					<div className='p-4'>
+						<p>
+							Границы, права, обязанности и ответственность - привилегия и очень
+							важные для человека вещи
+						</p>
+					</div>
+				</div>
+				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
+					<div className='p-4'>
 						<p>
 							Текст карусели, рамочный текст, новый удобный компонент,
 							листающийся за пределы экрана
@@ -15,7 +36,6 @@ const CarouselBeyond = () => {
 				</div>
 				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
 					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
 						<p>
 							Текст карусели, рамочный текст, новый удобный компонент,
 							листающийся за пределы экрана
@@ -24,7 +44,6 @@ const CarouselBeyond = () => {
 				</div>
 				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
 					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
 						<p>
 							Текст карусели, рамочный текст, новый удобный компонент,
 							листающийся за пределы экрана
@@ -33,34 +52,6 @@ const CarouselBeyond = () => {
 				</div>
 				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
 					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
-						<p>
-							Текст карусели, рамочный текст, новый удобный компонент,
-							листающийся за пределы экрана
-						</p>
-					</div>
-				</div>
-				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
-					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
-						<p>
-							Текст карусели, рамочный текст, новый удобный компонент,
-							листающийся за пределы экрана
-						</p>
-					</div>
-				</div>
-				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
-					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
-						<p>
-							Текст карусели, рамочный текст, новый удобный компонент,
-							листающийся за пределы экрана
-						</p>
-					</div>
-				</div>
-				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
-					<div className='p-4'>
-						<h3 className='text-2xl font-thin'>Заголовок</h3>
 						<p>
 							Текст карусели, рамочный текст, новый удобный компонент,
 							листающийся за пределы экрана
