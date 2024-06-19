ae555d7fc8e596dd85806a5903666631b0c0bcc3
tarchevsky <i.tarchevsky@gmai.com>
2024-06-19 12:50:04 +0300

    Забор изображения поста из базы с данными (данные поста: дата, рубрика пока не подтягиваются, только картинка, но они все базово свёрстаны) и добавлена функция типографической верстки tailwind вместе с уже настроенным prose и blockquote на странице. Это [postSlug].tsx, package.json, tailwind.config.js соответственно

--- pages/posts/[postSlug].tsx
+++ pages/posts/[postSlug].tsx
@@ -14,37 +14,68 @@ export default function Post({ post, site }: PostPageProps) {
 		<div>
 			<Meta seo={seo} />
 			<FadeIn className='relative'>
-				<img
-					src={featuredImage.node.link}
-					alt={featuredImage.node.altText}
-					className='h-[90svh] w-full object-cover brightness-50'
-				/>
-				<Link
-					href='/'
-					className='absolute top-[16px] left-[16px] sm:top-[40px] sm:left-[40px] md:top-[40px] md:left-[100px] 2xl:left-[200px]  text-white'
-				>
-					&lt; Назад
-				</Link>
-				<div className='absolute bottom-[16px] left-[16px] sm:bottom-[40px] sm:left-[40px] md:bottom-[40px] md:left-[100px] 2xl:left-[200px] flex flex-col'>
-					<h1 className='text-white'>{post.title}</h1>
-					<div className='text-white'>
-						Дата: <span>10.10.2010</span>
-					</div>
-					<div className='text-white'>
-						Рубрика: <span>Блог</span>
-					</div>
-				</div>
-			</FadeIn>
-			<FadeIn className='px-[16px] md:px-[400px]'>
 				<main>
-					<div>
-						<div
-							dangerouslySetInnerHTML={{
-								__html: post.content
-							}}
-						/>
-					</div>
+					{featuredImage ? (
+						<>
+							<img
+								src={featuredImage.node.link}
+								alt={featuredImage.node.altText}
+								className='h-[90svh] w-full object-cover brightness-50'
+							/>
+							<Link
+								href='/'
+								className='absolute top-[16px] left-[16px] sm:top-[40px] sm:left-[40px] md:top-[40px] md:left-[100px] 2xl:left-[200px]  text-white'
+							>
+								&lt; Назад
+							</Link>
+							<div className='absolute bottom-[16px] left-[16px] sm:bottom-[40px] sm:left-[40px] md:bottom-[40px] md:left-[100px] 2xl:left-[200px] flex flex-col'>
+								<h1 className='text-white'>{post.title}</h1>
+								<div className='text-white'>
+									Дата: <span>10.10.2010</span>
+								</div>
+								<div className='text-white'>
+									Рубрика: <span>Блог</span>
+								</div>
+							</div>
+						</>
+					) : (
+						<>
+							<img
+								src='/more-dark.jpg'
+								alt='Картинка статьи'
+								className='h-[90svh] w-full object-cover brightness-50'
+							/>
+							<Link
+								href='/'
+								className='absolute top-[16px] left-[16px] sm:top-[40px] sm:left-[40px] md:top-[40px] md:left-[100px] 2xl:left-[200px]  text-white'
+							>
+								&lt; Назад
+							</Link>
+							<div className='absolute flex flex-col'>
+								<h1 className='text-white'>{post.title}</h1>
+								<div className='text-white'>
+									Дата: <span>10.10.2010</span>
+								</div>
+								<div className='text-white'>
+									Рубрика: <span>Блог</span>
+								</div>
+							</div>
+						</>
+					)}
 				</main>
+			</FadeIn>
+			<FadeIn className='px-[16px]'>
+				<section className='prose m-auto'>
+					<blockquote>
+						<p>Цитата</p>
+					</blockquote>
+
+					<div
+						dangerouslySetInnerHTML={{
+							__html: post.content
+						}}
+					/>
+				</section>
 			</FadeIn>
 		</div>
 	)
