9082488eab7b12203ef551c32d93985f03ffe470
tarchevsky <i.tarchevsky@gmai.com>
2024-06-19 19:19:24 +0300

    CarouselBeyound - поправил отступы с xl на 2xl

--- src/components/carouselBeyond/CarouselBeyond.tsx
+++ src/components/carouselBeyond/CarouselBeyond.tsx
@@ -3,7 +3,7 @@ import FadeIn from '@/components/fadeIn/FadeIn'
 const CarouselBeyond = () => {
 	return (
 		<FadeIn className='mb-60'>
-			<div className='carousel carousel-center flex p-4 gap-5 mb-60 pl-4 sm:pl-10 md:pl-[100px] xl:pl-[200px] pr-4 sm:pr-10 md:pr-[100px] xl:pr-[200px]'>
+			<div className='carousel carousel-center flex p-4 gap-5 mb-60 pl-4 sm:pl-10 md:pl-[100px] 2xl:pl-[200px] pr-4 sm:pr-10 md:pr-[100px] 2xl:pr-[200px]'>
 				<div className='carousel-item w-3/5 shadow-lg rounded-box bg-base-200'>
 					<div className='p-4'>
 						<p>Выздоравливает не умный, а дисциплинированный</p>
